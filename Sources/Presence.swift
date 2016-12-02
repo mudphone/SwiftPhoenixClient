91e8094aba6dc017a7c4926a83c07542eaa3b353
Kyle Oba <koba@mudphone.com>
2016-12-01 14:35:07 -1000

    Add first update of syncDiff

--- Sources/Presence.swift
+++ Sources/Presence.swift
@@ -7,56 +7,72 @@ import Swift
 
 public class Presence {
   typealias Dictionary = [String: AnyObject]
-  typealias Callback = Void -> AnyObject
+  typealias State = [String: Dictionary]
+  typealias Callback = (String, Dictionary, Dictionary) -> Void
   
   /**
    Used to sync the list of presences on the server with the client
+   
+   Collect the new "joins" and "leaves" (and associated metadata),
+   and pass to `syncDiff`. For more info on the Presence datastructure
+   see: https://hexdocs.pm/phoenix/Phoenix.Presence.html#list/2
    
    - parameter state:    existing list of presences
    - parameter newState: new list of presences
    - parameter onJoin:   join callback
    - parameter onLeave:  leave callback
    */
-  func syncState(state: Dictionary, newState: Dictionary, onJoin: Callback, onLeave: Callback) {
-    var joins = [Dictionary]()
-    var leaves = [Dictionary]()
+  func syncState(state: State, newState: State, onJoin: Callback, onLeave: Callback) {
+    var joins  = State()
+    var leaves = State()
     
     for (key, presence) in state {
       if (newState[key] == nil) {
-        leaves.append([key: presence])
+        leaves[key] = presence
       }
     }
     
     for (key, newPresence) in newState {
       if let currentPresence = state[key] {
-        let newRefs = refs(newPresence as! Dictionary)
-        let curRefs = refs(currentPresence as! Dictionary)
-        let joinedMetas = metas(newPresence as! Dictionary).filter { m
-          in curRefs.indexOf(m["phx_ref"] as! String) < 0
+        let newRefs = refs(newPresence)
+        let curRefs = refs(currentPresence)
+        let joinedMetas = metas(newPresence).filter {
+          if let ref = $0["phx_ref"] as? String {
+            return !curRefs.contains(ref)
+          }
+          else { return false }
         }
-        let leftMetas = metas(currentPresence as! Dictionary).filter { m in
-          newRefs.indexOf(m["phx_ref"] as! String) < 0
+        let leftMetas = metas(currentPresence).filter {
+          if let ref = $0["phx_ref"] as? String {
+            return !newRefs.contains(ref)
+          }
+          else { return false }
         }
         
         if joinedMetas.count > 0 {
-          joins[key] = newPresence
-          joins[key]["metas"] = joinedMetas
+          var upd = newPresence
+          upd["metas"] = joinedMetas as AnyObject?
+          joins[key] = upd
         }
         
         if leftMetas.count > 0 {
-          leaves[key] = currentPresence
-          leaves[key]["metas"] = leftMetas
+          var upd = currentPresence
+          upd["metas"] = leftMetas as AnyObject?
+          leaves[key] = upd
         }
+      } else {
+        joins[key] = newPresence
       }
     }
+    return syncDiff(state: state, joins: joins, leaves: leaves, onJoin: onJoin, onLeave: onLeave)
   }
   
-  func refs(presence: Dictionary) -> [String] {
+  func refs(_ presence: Dictionary) -> [String] {
     let meta = metas(presence)
     return meta.map { m in m["phx_refs"]! as! String }
   }
   
-  func metas(presence: Dictionary) -> [Dictionary] {
+  func metas(_ presence: Dictionary) -> [Dictionary] {
     if let meta = presence["metas"] {
       return meta as! [Dictionary]
     } else {
@@ -73,7 +89,7 @@ public class Presence {
    - parameter onJoin:  join callback
    - parameter onLeave: leave callback
    */
-  func syncDiff(state: Dictionary, joins: Dictionary, leaves: Dictionary, onJoin: Callback, onLeave: Callback) {
+  func syncDiff(state: State, joins: State, leaves: State, onJoin: Callback, onLeave: Callback) {
     
   }
   
